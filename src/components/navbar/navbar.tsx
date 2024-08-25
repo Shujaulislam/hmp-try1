94926977ca97435d896e6c6e9fba7de7604ef437
Huzaif Akhtar <huzaif2025@gmail.com>
2024-08-26 02:34:54 +0530

    chore: Update footer and navbar styles

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -54,8 +54,8 @@ const Navbar = () => {
                             </div>
                         </Link>
                     </div>
-                    <div className={`flex flex-col space-y-10 inset-0 fixed top-0  h-[100vh] bg-white dark:bg-gray-950 lg:!bg-transparent py-20 px-5 sm:px-10 md:px-14 transition-all ease-linear duration-300 lg:flex-row lg:flex-1 lg:py-0 lg:px-0 lg:space-y-0 lg:gap-x-10 lg:relative lg:top-0 lg:h-full lg:items-center lg:justify-between lg:w-max ${openNavbar ? "visible opacity-100 translate-y-0" : "-translate-y-9 opacity-0 invisible lg:translate-y-0 lg:visible lg:opacity-100"}`}>
-                        <ul className="flex flex-col gap-y-5 text-white lg:items-center lg:flex-row lg:gap-x-5 lg:h-full lg:justify-center lg:flex-1">
+                    <div className={`flex flex-col space-y-10 inset-0 fixed top-0  h-[100vh] bg-white  dark:bg-gray-950 lg:!bg-transparent py-20 px-5 sm:px-10 md:px-14 transition-all ease-linear duration-300 lg:flex-row lg:flex-1 lg:py-0 lg:px-0 lg:space-y-0 lg:gap-x-10 lg:relative lg:top-0 lg:h-full lg:items-center lg:justify-between lg:w-max ${openNavbar ? "text-black text-center text-xl visible opacity-100 translate-y-0" : "-translate-y-9 text-white opacity-0 invisible lg:translate-y-0 lg:visible lg:opacity-100"}`}>
+                        <ul className="flex flex-col gap-y-5  lg:items-center lg:flex-row lg:gap-x-5 lg:h-full lg:justify-center lg:flex-1">
                             <li>
                                 <Link href="/" className="transition ease-linear hover:text-gray-200">
                                     Home
@@ -89,13 +89,13 @@ const Navbar = () => {
                         </div>
                     </div>
                     <div className="flex items-center justify-end relative z-60 lg:hidden">
-                        <button onClick={toggleNavbar} className="p-3 rounded-full bg-emerald-600 dark:bg-emerald-500 outline-none w-12 aspect-square flex flex-col relative justify-center items-center">
+                        <button onClick={toggleNavbar} className="p-3 rounded-full outline-none w-12 aspect-square flex flex-col relative justify-center items-center">
                             <span className="sr-only">
                                 Toggle navbar
                             </span>
-                            <span className={`w-6 h-0.5 rounded-full bg-gray-300 transition-transform duration-300 ease-linear ${openNavbar ? "translate-y-1.5 rotate-[40deg]" : ""}`} />
-                            <span className={`w-6 origin-center mt-1 h-0.5 rounded-full bg-gray-300 transition-all duration-300 ease-linear ${openNavbar ? "scale-x-0 opacity-0" : ""}`} />
-                            <span className={`w-6 mt-1 h-0.5 rounded-full bg-gray-300 transition-all duration-300 ease-linear ${openNavbar ? "-translate-y-1.5 -rotate-[40deg]" : ""}`} />
+                            <span className={`w-6 h-0.5 rounded-full bg-orange-500 transition-transform duration-300 ease-linear ${openNavbar ? "translate-y-1.5 rotate-[40deg]" : ""}`} />
+                            <span className={`w-6 origin-center mt-1 h-0.5 rounded-full bg-orange-500 transition-all duration-300 ease-linear ${openNavbar ? "scale-x-0 opacity-0" : ""}`} />
+                            <span className={`w-6 mt-1 h-0.5 rounded-full bg-orange-500 transition-all duration-300 ease-linear ${openNavbar ? "-translate-y-1.5 -rotate-[40deg]" : ""}`} />
                         </button>
                     </div>
                 </nav>
