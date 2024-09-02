08908f78cd696aad3b04cf9c0fdb57dac9a7c271
SHUJA <106554018+Shujaulislam@users.noreply.github.com>
2024-09-02 13:39:34 +0530

    footer updated

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,4 +1,7 @@
+"use client"
 import Link from "next/link"
+import { useState, useEffect, useRef } from 'react';
+import { FaInstagram, FaLinkedin } from 'react-icons/fa';
  
 const FooterItem = ({ text, link }: { text: string; link: string }) => {
 return (
@@ -30,13 +33,50 @@ const footerItems = [
  
  
 const FooterBlock = () => {
+    const [footerClass, setFooterClass] = useState('bg-transparent');
+    const footerRef = useRef<HTMLElement>(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        setFooterClass('bg-black');
+                    } else {
+                        setFooterClass('bg-transparent text-black');
+                    }
+                });
+            },
+            { threshold: 0.1 } // Adjust the threshold as needed
+        );
+
+        if (footerRef.current) {
+            observer.observe(footerRef.current);
+        }
+
+        return () => {
+            if (footerRef.current) {
+                observer.unobserve(footerRef.current);
+            }
+        };
+    }, []);
+
 return (
-    <footer className="footer footer-center text-center text-white bg-black text-base-content p-4">
-  <aside className="flex justify-center">
-    <p>
-        Copyright © {new Date().getFullYear()} - All right reserved by <br className="md:hidden" /> <span className="text-orange-500 md:hover:text-orange-500 md:text-white"> Humaya Power </span></p>
-  </aside>
-</footer>
+   <footer ref={footerRef} className={`footer footer-center text-center text-white text-base-content p-4 transition-all duration-300 ${footerClass}`}>
+            <aside className="flex flex-col items-center">
+                <p className="mb-4">
+                    Copyright © {new Date().getFullYear()} - All right reserved by <br className="md:hidden" /> <span className="text-orange-500 md:hover:text-orange-500 md:text-white"> Humaya Power </span>
+                </p>
+                <div className="flex space-x-4">
+                    <Link href="https://www.instagram.com/humayapower?igsh=NTc4MTIwNjQ2YQ==" target="_blank" rel="noopener noreferrer">
+                        <FaInstagram className="text-white hover:text-gray-400" size={24} />
+                    </Link>
+                    <Link href="https://www.linkedin.com/company/humaya-power/" target="_blank" rel="noopener noreferrer">
+                        <FaLinkedin className="text-white hover:text-gray-400" size={24} />
+                    </Link>
+                </div>
+            </aside>
+        </footer>
 )
 }
  
